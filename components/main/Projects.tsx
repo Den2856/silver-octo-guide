f18db87e34fc1dd6c62a181889d01ff4805de257
Den2856 <168768650+Den2856@users.noreply.github.com>
2025-03-26 13:40:32 +0300

    Update Projects.tsx

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -12,6 +12,12 @@ const Projects = () => {
       </h1>
       <div className="h-full w-full grid grid-cols-3  md:flex-row gap-10 px-10">
         <ProjectCard
+          src="/projects/12.png"
+          link="https://solid-guacamole-blond.vercel.app/"
+          title="CarHub"
+          description="Find, book, rent a car—quick and super easy! Streamline your car rental experience with our effortless booking process."
+        />
+        <ProjectCard
           src="/projects/1.png"
           link="https://den2856.github.io/legendary-palm-tree/jadoo/public/index.html"
           title="Jadoo: Travel with Ease."
